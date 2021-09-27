6d40574fa2dd5d20146de59f50ffa0fa2d6ca20c
RomelVazquez2008 <55199380+RomelVazquez2008@users.noreply.github.com>
2021-09-27 18:17:37 -0500

    Ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,28 @@
+/*
+INSTRUCCIONES:
+HAZ QUE EL SIGUIENTE PROGRAMA FUNCIONE SIN ERRORES
+Y DESPIEGLE EL RESULTADO ESPERADO
+*/
+
+/*
+TIP:
+HAZ USO DE LO APRENDIDO EN:
+COMENTARIOS
+DESPLEGAR ALGO EN PANTALLA
+OPERADORES
+VARIABLES Y CONSTANTES
+*/
+
+
+El siguiente programa implementa la formula
+para obtener el area de un circulo,
+la cual será desplegada en pantalla
+
+
+// var area
+const PI=1.5708 x 2
+const RADIO=2
+
+area= pi x radio x radio
+
+consola.o(area)
